ecb004ee1badf6a82b04ddfc17916c4445056f6f
Ayan0k0uji <101419149+Ayan0k0uji@users.noreply.github.com>
2022-09-30 09:59:39 +0400

    доделал search

--- models/event-model.js
+++ models/event-model.js
@@ -68,6 +68,12 @@ Event.searchAll = async function (query) {
         });
     }
 
+    if (query.price_from && query.price_to) {
+        whereStatement[Op.and].push({
+            price: {[Op.between]: [query.price_from, query.price_to]}
+        });
+    }
+
     if (query.city) {
         whereStatement[Op.and].push({
             city: query.city
