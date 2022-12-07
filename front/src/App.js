5717d51b65b1e8f1225ee0cee06d7433b328602c
Dmirty Kiselev <kiselevzfth@gmail.com>
2022-12-07 23:33:58 +0400

    смержил весь проект перед хостингом

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import Layout from './pages/layout';
 import EventPage from './pages/event';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventReg from './pages/eventReg';
+import EventRegOrg from './pages/eventRegOrg';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" element={<Layout />}/>
           <Route exact path="/events/:id" element={<EventPage /> }/>
           <Route path="/eventReg" element={<EventReg /> }/>
+          <Route path="/eventRegOrg" element={<EventRegOrg /> }/>
          </Routes>
       </Router>
       
