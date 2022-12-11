801b25610f2f10d6287b97727fe4d11e9b09511e
Dmirty Kiselev <kiselevzfth@gmail.com>
2022-12-11 23:01:49 +0400

    добавил календарь

--- front/src/components/date/index.js
+++ front/src/components/date/index.js
@@ -1,7 +1,11 @@
 import s from './s.module.scss';
-import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
+//import './calendar.css';
+import './calendar.scss'
+import { CaretDownOutlined, CaretUpOutlined, ConsoleSqlOutlined } from '@ant-design/icons';
 import React, { useRef } from "react";
+import { useState } from "react";
 import { useDetectOutsideClick } from "./useDetectOutsideClick";
+import Calendar from 'react-calendar';
 
 
 
@@ -9,16 +13,27 @@ export const Date = () => {
     const dropdownRef = useRef(null);
     const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
     const onClick = () => setIsActive(!isActive);
+
+    //const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState();
+
+    const prnt = () => {
+        console.log(date);
+    }
+
     return (
-        <div className={s.date}>
+        <div className={s.date} ref={dropdownRef}>
 
             <button className={s.dateButton}  onClick={onClick}> 
                 <div className = {s.textDate}>Дата </div>
-                <div className = {s.iconDate}> {isActive ? <CaretUpOutlined /> : <CaretDownOutlined />} </div>
+                <div className = {isActive ? s.iconDateActive : s.iconDateInactive}> <CaretDownOutlined /> </div>
             </button>
 
             <div className={isActive ? s.calendarActive : s.calendar}>
-                {/*  */}
+                <Calendar 
+                    onChange={setDate}
+                    value={date}
+                    selectRange={true}  />
             </div>
         </div>
     );
